6d1e3afd731dedfe4e82f263eb4f66f3cc8ea756
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-01-31 01:07:39 +0800

    fetching data from api and presenting in a table

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,3 +1,55 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { onMount } from "svelte";
+
+    let children = null
+    let naughtiestChild = null
+    let nicestChild = null
+
+    async function fetchData() {
+        try {
+            const response = await fetch('https://advent.sveltesociety.dev/data/2023/day-one.json')
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`)
+            }
+            children = await response.json()
+        }   catch (error) {
+            console.error('Fetch error:', error)
+        }
+    }
+
+    $: if (children) {
+        children.sort((a,b) => b.tally - a.tally)
+
+        if (children.length > 0) {
+            nicestChild = children[0]
+            naughtiestChild = children[children.length - 1]
+        }
+    }
+
+    onMount(() => {
+        fetchData()
+    })
+</script>
+
+<div>
+<h1>Naughty or Nice</h1>
+{#if children}
+<table>
+    {#each children as child, index}
+    <tr>
+        <td>
+            {index + 1}
+        </td>
+        <td>
+            {child.name}
+        </td>
+        <td>
+            {child.tally}
+        </td>
+    </tr>
+    {/each}
+</table>
+{:else}
+    <p>Loading...</p>
+{/if}
+</div>
