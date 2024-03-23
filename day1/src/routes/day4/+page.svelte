9c3e5e725de0b14e0af20abd24931a2629921afe
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-24 02:04:08 +0800

    fixed bug with the graph for day4, changed styling a bit

--- day1/src/routes/day4/+page.svelte
+++ day1/src/routes/day4/+page.svelte
@@ -1,15 +1,40 @@
 <script lang="ts">
     import { onMount } from 'svelte'
     import { Line } from 'svelte-chartjs'
-    import { type ChartData, type ChartOptions } from 'chart.js'
+    import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    LinearScale,
+    PointElement,
+    CategoryScale,
+    Chart,
+  } from 'chart.js'
+
+    ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    LinearScale,
+    PointElement,
+    CategoryScale,
+  );
+
+    ChartJS.defaults.color = 'rgb(255, 255, 255)'
 
     let data: number[] = []
+
     $: myData = {
         labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
         datasets: [{
-            label: 'hi',
+            label: 'heart rate',
             data: data,
-            borderWidth: 1
+            borderWidth: 1,
+            borderColor: 'rgb(255, 255, 255)',
+            backgroundColor: 'rgb(255, 255, 255)'
         }]
     }
 
@@ -28,5 +53,5 @@
 
 <div>
     {JSON.stringify(data)}
-    <Line data={myData} width={100} height={200}/>
+    <Line data={myData} width={100} height={50} options={{ animation: false }}/>
 </div>
