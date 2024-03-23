5c3539646c6f1e65bbe2a741c1c82e402d92d06b
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-23 20:59:22 +0800

    file management for different days, page.server.ts to fetch from api for day3

---
+++ day1/src/routes/day3/+page.server.ts
@@ -0,0 +1,8 @@
+export const load = async ({ fetch }) => {
+    const response = await fetch('https://advent.sveltesociety.dev/data/2023/day-three.json')
+    const data = await response.json()
+
+    return {
+        data
+    }
+}
