7d209a02ebc01470d80e539125b1703947a017fb
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-24 10:22:21 +0800

    day5: setInterval to fetch data every second

---
+++ (days)/src/routes/day5/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { onMount } from "svelte"
+
+    type TaskType = 'CREATED_TOY' | 'WRAPPED_PRESENT'
+
+    interface Task {
+        elf: string
+        task: TaskType
+        minutesTaken: number
+        date: string;
+    }
+
+    let tasks: Task[]
+
+    onMount(async () => {
+        setInterval(async () => {
+            const response = await fetch('https://advent.sveltesociety.dev/data/2023/day-five.json')
+            tasks = await response.json()
+        }, 1000)
+    })
+</script>
+
+<div>
+    {JSON.stringify(tasks)}
+</div>
