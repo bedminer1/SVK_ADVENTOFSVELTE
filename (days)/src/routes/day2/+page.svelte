a958cc5f8ffb325d63420b2f520972ce2c798d75
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-24 21:24:08 +0800

    simple counter with increment, decrement, and reset for day2

---
+++ (days)/src/routes/day2/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    let count = 0
+    $: increment = () => {
+        count++
+    }
+    $: decrement = () => {
+        count--
+    }
+    $: reset = () => {
+        count = 0
+    }
+</script> 
+
+<div>
+    {count}
+    <button on:click={increment}>add</button>
+    <button on:click={decrement}>reduce</button>
+    <button on:click={reset}>reset</button>
+</div>
