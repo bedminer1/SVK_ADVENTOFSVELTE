9f6faa83e4e685b026550c9ba9e9dd24126daf24
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-25 23:24:18 +0800

    created timer and got bpm, added reset button

--- (days)/src/routes/day6/+page.svelte
+++ (days)/src/routes/day6/+page.svelte
@@ -1,17 +1,47 @@
 <script lang="ts">
-    let counter = 0
+    import { onMount } from 'svelte'
 
+    let counter = 0
+    let resetBtn: HTMLButtonElement
+
+    
+    let date = new Date()
+    
+    onMount(() => setInterval(() => {
+        date = new Date()
+    }, 1000))
+    
+    let startingTime = timeToSeconds(date.toLocaleTimeString())
+    $: time = date.toLocaleTimeString()
+    
+    function timeToSeconds(time: string): number {
+        const timeArr = time.split(':')
+        let output: number = 0
+        output += Number(timeArr[1]) * 60
+        output += Number(timeArr[2])
+        output += Number(timeArr[0]) * 60 * 60
+        return output
+    }
+    
+    $: currSeconds = timeToSeconds(time)
+    $: timePassed = currSeconds - startingTime
     $: increment = () => {
         counter++
     }
 
     $: reset = () => {
         counter = 0
+        startingTime = timeToSeconds(date.toLocaleTimeString())
+        setTimeout(() => {
+            resetBtn.blur()
+        }, 200);
     }
 </script>
 
-<p>{counter}</p>
+{#if counter > 0}
+    {(counter / (timePassed / 60)).toFixed(1)}
+{/if}
 <svelte:window on:keydown={increment} />
 <p>tap any key to record beat</p>
-<button class="btn" on:click={reset}>click when song over</button>
+<button class="btn" on:click={reset} bind:this={resetBtn}>click when song over</button>
 
