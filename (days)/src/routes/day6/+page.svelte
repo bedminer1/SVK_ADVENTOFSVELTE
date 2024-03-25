2751b6336b336b32f5738f3dd827159d4e5b5762
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-25 22:30:46 +0800

    basic counter to keep track of beats, reset button

---
+++ (days)/src/routes/day6/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    let counter = 0
+
+    $: increment = () => {
+        counter++
+    }
+
+    $: reset = () => {
+        counter = 0
+    }
+</script>
+
+<p>{counter}</p>
+<svelte:window on:keydown={increment} />
+<p>tap any key to record beat</p>
+<button class="btn" on:click={reset}>click when song over</button>
+
