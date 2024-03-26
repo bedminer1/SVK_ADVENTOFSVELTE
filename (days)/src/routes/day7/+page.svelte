512e2a1cf62fcbf52208993dcf3f823644b12dd5
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-26 22:09:14 +0800

    using a map to produce a new string encoded via morsecode

---
+++ (days)/src/routes/day7/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    const morseCode = new Map([
+    ["A", ".-"],
+    ["B", "-..."],
+    ["C", "-.-."],
+    ["D", "-.."],
+    ["E", "."],
+    ["F", "..-."],
+    ["G", "--."],
+    ["H", "...."],
+    ["I", ".."],
+    ["J", ".---"],
+    ["K", "-.-"],
+    ["L", ".-.."],
+    ["M", "--"],
+    ["N", "-."],
+    ["O", "---"],
+    ["P", ".--."],
+    ["Q", "--.-"],
+    ["R", ".-."],
+    ["S", "..."],
+    ["T", "-"],
+    ["U", "..-"],
+    ["W", ".--"],
+    ["X", "-..-"],
+    ["Y", "-.--"],
+    ["Z", "--.."],
+    [" ", "  "],
+    ])
+
+    let input: string = ''
+
+    function encode(input: string): string {
+        const str: string = input.toUpperCase()
+        const res: string[] = []
+
+        for (const char of str) {
+            res.push(morseCode.get(char) || '')
+        }
+        return res.join(' ')
+    }
+    
+    $: encodedInput = encode(input)
+</script>
+
+
+<p class="text-sm">
+    encoded: <pre>{encodedInput}</pre>
+
+<form class="w-1/2">
+    <input type="text" class="input w-1/2 rounded-sm" bind:value={input}>
+</form>
