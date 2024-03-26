ca6b31b288e071b721f09424aafe52bb6188406c
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-26 18:23:20 +0800

    made it so that you would have to start clicking for the timer to actually start instead of starting onMount

--- (days)/src/routes/day6/+page.svelte
+++ (days)/src/routes/day6/+page.svelte
@@ -5,43 +5,35 @@
     let resetBtn: HTMLButtonElement
 
     
-    let date = new Date()
+
     
-    onMount(() => setInterval(() => {
-        date = new Date()
-    }, 1000))
     
-    let startingTime = timeToSeconds(date.toLocaleTimeString())
-    $: time = date.toLocaleTimeString()
-    
-    function timeToSeconds(time: string): number {
-        const timeArr = time.split(':')
-        let output: number = 0
-        output += Number(timeArr[1]) * 60
-        output += Number(timeArr[2])
-        output += Number(timeArr[0]) * 60 * 60
-        return output
-    }
-    
-    $: currSeconds = timeToSeconds(time)
-    $: timePassed = currSeconds - startingTime
+    let startingTime: number
+    let currSeconds: number
+    setInterval(() => {
+        currSeconds = Date.now()
+    }, 500)
+    $: timePassed = (currSeconds - startingTime) / 1000
+
     $: increment = () => {
+        if (!startingTime) startingTime = Date.now()
         counter++
     }
 
     $: reset = () => {
         counter = 0
-        startingTime = timeToSeconds(date.toLocaleTimeString())
+        startingTime = Date.now()
         setTimeout(() => {
             resetBtn.blur()
         }, 200);
     }
 </script>
 
-{#if counter > 0}
-    {(counter / (timePassed / 60)).toFixed(1)}
+{#if counter > 0 && !isNaN(timePassed)}
+    BPM: {(counter / (timePassed / 60)).toFixed(1)}
+{:else}
+    <p>tap any key to start</p>
 {/if}
 <svelte:window on:keydown={increment} />
-<p>tap any key to record beat</p>
 <button class="btn" on:click={reset} bind:this={resetBtn}>click when song over</button>
 
