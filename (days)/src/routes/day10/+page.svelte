2f1e420dd7521a36b6c54dbf6325a409e432d3e2
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-27 21:56:51 +0800

    simple modal, TO DO add background opacity

---
+++ (days)/src/routes/day10/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+    let showPopup = true
+
+    function closePopup() {
+        showPopup = false
+    }
+</script>
+
+<div class="w-full flex flex-col justify-center items-center h-[80vh]">
+{#if showPopup} 
+    <div class="card p-5 rounded-sm variant-filled-primary">
+        <p>hi</p>
+        <button on:click={() => showPopup = false}>close</button>
+    </div>
+{:else}
+    <button on:click={() => showPopup = true}>show</button>
+{/if}
+</div>
