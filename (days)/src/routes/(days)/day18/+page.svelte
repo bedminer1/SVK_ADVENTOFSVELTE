e857a3268a7564e6d01ce9b73b94276afeae0d8c
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-31 18:44:36 +0800

    a bit of styling for day 18

--- (days)/src/routes/(days)/day18/+page.svelte
+++ (days)/src/routes/(days)/day18/+page.svelte
@@ -20,7 +20,7 @@
 <div class="flex flex-col w-1/2 items-center justify-center gap-4">
     {#if !finished}
     {#each data.quiz as question, index}
-        <div class="card w-full p-4 h-42">
+        <div class="card w-full p-4 h-42 max-w-96 h-40 flex flex-col justify-around">
             <p>{index + 1}. {question.question}</p>
             <select name="" id="" class="select w-1/2 mt-2" bind:value={answers[index]}>
                 <option value="" disabled selected>Select an Option</option>
@@ -30,10 +30,12 @@
             </select>
         </div>
         {/each}
-        <button on:click={() => calculateScore()}>Submit</button>
+        <button class="btn bg-green-800 rounded-md" on:click={() => calculateScore()}>Submit</button>
     {:else}
-        <p>Score: {score}/20</p>
-        <button on:click={() => finished = false}>Retry</button>
+    <div class="flex flex-col items-center">
+        <p class="mb-3">Score: {score}/20</p>
+        <button on:click={() => finished = false} class="btn bg-green-800 rounded-lg p-2">Retry</button>
+    </div>
     {/if}
 </div>
 
