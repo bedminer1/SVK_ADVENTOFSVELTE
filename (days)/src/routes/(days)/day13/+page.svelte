3d0bd98258b0f0ac58c9da26866c49027ad3d943
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-31 16:28:40 +0800

    used accordion component from skeleton, displays weight, number of children and children inside accordion as cards on css grid

--- (days)/src/routes/(days)/day13/+page.svelte
+++ (days)/src/routes/(days)/day13/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+
     export let data
     let count = 0
 
@@ -16,5 +18,29 @@
             count = 0
         }
     }
-    console.log(allBags)
 </script>
+
+<div class="flex flex-col items-center w-full">
+    <h1 class="h1 text-center mt-10 mb-2">Sleigh Load Balancer 2.0</h1>
+    <p class="p mb-10">Automatically sorted for your convenience :-&#41</p>
+    <div class="w-1/2">
+        <Accordion>
+            {#each allBags as presentGroup, index}
+            <AccordionItem>
+                <svelte:fragment slot="lead">Bag ID: {index + 1}</svelte:fragment>
+                <svelte:fragment slot="summary">
+                    Number of Children: {presentGroup.presents.length} | Weight: {presentGroup.weight.toFixed(2)}kg
+                </svelte:fragment>
+                <svelte:fragment slot="content">
+                    <div class="grid grid-cols-3 gap-4">
+                        {#each presentGroup.presents as present, index}
+                            <p class="rounded-lg bg-green-800 p-3 hover:bg-green-900">{index + 1}. Name: {present.name} | Present Weight: {present.weight}kg</p>
+                        {/each}
+                    </div>
+                    
+                </svelte:fragment>
+            </AccordionItem>
+            {/each}
+        </Accordion>
+    </div>
+</div>
