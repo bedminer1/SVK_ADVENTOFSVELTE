74b84e55dfa3ae5091d66fa91a43a8f1173c1ef9
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-04-01 17:55:58 +0800

    establish function to count number of snowflakes and spawn them in with attributes that will be controlled

--- (days)/src/lib/components/snowfall.svelte
+++ (days)/src/lib/components/snowfall.svelte
@@ -15,4 +15,39 @@
     let height = 0
     let lastUpdate: number
     let frameHandle: number
+
+    onMount(() => {
+        lastUpdate = Date.now()
+        loop()
+
+        return () => cancelAnimationFrame(frameHandle)
+    })
+
+    // returns random number between min and max
+    function uniform(min: number, max: number): number {
+        return min + Math.random() * (max - min)
+    }
+
+    function loop() {
+        // calculate time between frames
+        const now = Date.now()
+        const delta = now - lastUpdate
+        lastUpdate = now
+
+        // count represents the number of snowflakes spawned per frame
+        const count = rate * uniform(0, 2) * delta / 1000
+        for (let i = 0; i < count; i++) {
+            particles.push({
+                // width represents the horizontal window where snowflake can spawn
+                x: uniform(-width / 2, width * 1.5),
+                // spawn at the top
+                y: -10,
+                // horizontal speed, + for right, - for left
+                vx: uniform(-driftVariance, driftVariance) + drift,
+                // vertical speed
+                vy: uniform(-gravityVariance, gravityVariance) + gravity,
+                size: Math.random() * size
+            })
+        }
+    }
 </script>
