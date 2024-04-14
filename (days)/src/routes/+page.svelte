a702b74157ab3258c9d0a1f3434e4ba061fa1ed6
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-04-14 17:14:58 +0800

    fixing layout on home page

--- (days)/src/routes/+page.svelte
+++ (days)/src/routes/+page.svelte
@@ -18,16 +18,17 @@
         days.push(i)
     }
 </script>
-
-<div class="flex flex-col h-screen items-center justify-center">
-    <h1 class="h1">Advent of Svelte</h1>
-    <h3 class="h3 mt-2 mb-10">
-        <a href="https://github.com/bedminer1/SVK_ADVENTOFSVELTE" target="_blank">by @bedminer1</a>
-    </h3>
-    <div class="grid grid-cols-4 items-center justify-center gap-2 w-1/3 overflow-auto mt-10">
-        {#each days as day}
-        <a href="/day{day}" class="text-center">Day {day}</a>
-        {/each}
+<div class="w-full flex justify-center">
+    <div class="flex flex-col h-screen items-center justify-center w-full xl:w-[75rem]">
+        <h1 class="h1">Advent of Svelte</h1>
+        <h3 class="h3 mt-2 mb-10">
+            <a href="https://github.com/bedminer1/SVK_ADVENTOFSVELTE" target="_blank">by @bedminer1</a>
+        </h3>
+        <div class="grid grid-cols-4 items-center justify-center gap-2 w-1/3 overflow-auto mt-10">
+            {#each days as day}
+            <a href="/day{day}" class="text-center">Day {day}</a>
+            {/each}
+        </div>
     </div>
 </div>
 
