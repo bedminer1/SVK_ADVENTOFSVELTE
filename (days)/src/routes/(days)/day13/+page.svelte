df844be1c62a710d06e52b43f51e25032192b492
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-04-15 23:44:59 +0800

    changed day13 to table with pagination for bag id instead of accordion

--- (days)/src/routes/(days)/day13/+page.svelte
+++ (days)/src/routes/(days)/day13/+page.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-    import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+    import { Accordion, AccordionItem, Paginator, Table } from '@skeletonlabs/skeleton';
+    import type { PaginationSettings } from '@skeletonlabs/skeleton';
 
     export let data
+    
     let count = 0
-
     let allBags: PresentGroup[] = []
     let bag: Present[] = []
+    let tableSource: Present[] = data.presents
 
+// GROUPING DATA
     for (let present of data.presents) {
         // add present to bag
         if (count + present.weight < 100) {
@@ -19,29 +22,70 @@
             count = 0
         }
     }
+    
+    // PAGINATION 
+        let displayedIndex = 1
+        $: displayedBag = allBags[displayedIndex - 1]
+        
+        const incrementDisplayIndex = () => {
+            displayedIndex++
+            scrollToBottom()
+        }
+        const decrementDisplayIndex = () => {
+            displayedIndex--
+            scrollToBottom()
+        }
+        const setStartDisplayIndex = () => {
+            displayedIndex = 1
+            scrollToBottom()
+        }
+        const setEndDisplayIndex = () => {
+            displayedIndex = allBags.length
+            scrollToBottom()
+        }
+
+// SCROLL
+        function scrollToBottom() {
+            setTimeout(() => {
+                window.scrollTo(0, 1200)
+            }, 200)
+        }
 </script>
 
 <div class="flex flex-col items-center w-full">
     <h1 class="h1 text-center mt-10 mb-2">Sleigh Load Balancer 2.0</h1>
     <p class="p mb-10">Automatically sorted for your convenience :-&#41</p>
     <div class="w-1/2">
-        <Accordion>
-            {#each allBags as presentGroup, index}
-            <AccordionItem>
-                <svelte:fragment slot="lead">Bag ID: {index + 1}</svelte:fragment>
-                <svelte:fragment slot="summary">
-                    Number of Children: {presentGroup.presents.length} | Weight: {presentGroup.weight.toFixed(2)}kg
-                </svelte:fragment>
-                <svelte:fragment slot="content">
-                    <div class="grid grid-cols-3 gap-4">
-                        {#each presentGroup.presents as present, index}
-                            <p class="rounded-lg bg-green-800 p-3 hover:bg-green-900">{index + 1}. Name: {present.name} | Present Weight: {present.weight}kg</p>
-                        {/each}
-                    </div>
-                    
-                </svelte:fragment>
-            </AccordionItem>
-            {/each}
-        </Accordion>
+        <table class="table">
+            <thead>
+                <th>Index</th>
+                <th>Name</th>
+                <th>Present Weight</th>
+            </thead>
+            <tbody>
+                {#each displayedBag.presents as present, index}
+                <tr>
+                        <td>{index + 1}</td>
+                        <td>{present.name}</td>
+                        <td>{present.weight.toFixed(2)}</td>
+                    </tr>
+                {/each}
+            </tbody>
+            <tfoot>
+                <td>Bag ID: {displayedIndex}</td>
+                <td>Number of Children: {displayedBag.presents.length}</td>
+                <td>Total Weight: {displayedBag.weight.toFixed(2)}</td>
+            </tfoot>
+        </table>
+
+
+
+        <div class="flex gap-3 py-3 w-full justify-center">
+            <button on:click={setStartDisplayIndex}>&#8592</button>
+            <button on:click={decrementDisplayIndex}>&#8592</button>
+            <p>{displayedIndex}</p>
+            <button on:click={incrementDisplayIndex}>&#8594;</button>
+            <button on:click={setEndDisplayIndex}>&#8594;</button>
+        </div>
     </div>
 </div>
