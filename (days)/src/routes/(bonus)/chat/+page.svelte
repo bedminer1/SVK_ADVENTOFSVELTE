1a6c4eb4948b73d6d2091e5ebe7d3500bd08f07e
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-05-02 02:07:03 +0800

    some styling adjustments for the chat

--- (days)/src/routes/(bonus)/chat/+page.svelte
+++ (days)/src/routes/(bonus)/chat/+page.svelte
@@ -40,6 +40,7 @@
                 {:else if texts.length > 1 && texts[texts.indexOf(text) - 1].author === text.author} 
                     <span></span>
                 {:else}
+                <hr class="my-2 w-1/2">
                     <div class="flex justify-between">
                         <p>{text.author}</p>
                         <p><i>{text.time.toLocaleDateString()} | {text.time.toLocaleTimeString().slice(0,5)}</i></p>
@@ -54,11 +55,11 @@
     <form action="?/send" method="POST" class="flex flex-col w-2/5 gap-2" on:submit={handleSubmit} use:enhance>
         <input type="text" class="input variant-ghost-primary rounded-lg" name="author" bind:value={$form.author} placeholder="Enter User">
         {#if $errors.author}
-            {$errors.author}
+            <p><i>Please enter a user</i></p>
         {/if}
         <input class="input variant-ghost-primary rounded-lg" name="body" type="text" bind:value={$form.body} placeholder="Enter Message">
         {#if $errors.body}
-            {$errors.body}
+            <p><i>Please enter a message</i></p>
         {/if}
         <button></button>
     </form>
