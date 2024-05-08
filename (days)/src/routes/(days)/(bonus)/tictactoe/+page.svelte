49a44a7ee0723f2687611104b589f3eaa8ecd195
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-05-09 02:36:01 +0800

    win detection and reset button

--- (days)/src/routes/(days)/(bonus)/tictactoe/+page.svelte
+++ (days)/src/routes/(days)/(bonus)/tictactoe/+page.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
     const board: string[][] = []
-    for (let r = 0; r < 3; r++) {
-        board[r] = []
-        for (let c = 0; c < 3; c++) {
-            board[r][c] = ""
+    function setUpBoard() {
+        for (let r = 0; r < 3; r++) {
+            board[r] = []
+            for (let c = 0; c < 3; c++) {
+                board[r][c] = ""
+            }
         }
     }
 
+    setUpBoard()
+
     let playerOneTurn: boolean = true
 
-    console.log(board)
     function handleClick(e: MouseEvent) {
         // @ts-ignore
         const [value, row, col] = (e.target.id).split(',')
@@ -26,10 +29,35 @@
             board[Number(row)][Number(col)] = 'O'
             playerOneTurn = true
         }
-    }
-</script>
 
-<div class="flex justify-center items-center w-full h-[90vh]">
+        const hasSomeoneWon = checkWin()
+        if (hasSomeoneWon === true) {
+            console.log("victory", hasSomeoneWon)
+        }
+    }
+
+    function checkWin(): boolean {
+        // CHECK ROWS
+        for (let r = 0; r < 3; r++) {
+            if (board[r][0] === board[r][1] && board[r][1] === board[r][2] && board[r][0] !== "") return true
+        }
+
+        // CHECK COLS
+        for (let c = 0; c < 3; c++) {
+            if (board[0][c] === board[1][c] && board[1][c] === board[2][c] && board[0][c] !== "") return true
+        }
+
+        // CHECK DIAGONAL
+        if (((board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== "")
+        || (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[2][0] !== ""))) {
+            return true
+        }
+        
+        return false
+    }
+</script> 
+
+<div class="flex flex-col justify-center items-center w-full h-[90vh] gap-10">
     <div class="grid grid-cols-3 w-80 text-5xl">
     {#each board as r, row}
         {#each r as cell, col}
@@ -39,4 +67,5 @@
         {/each}
     {/each}
     </div>
+    <button class="btn variant-ghost-primary rounded-xl" on:click={() => setUpBoard()}>RESET</button>
 </div>
