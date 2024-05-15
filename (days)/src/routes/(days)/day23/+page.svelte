fea9147a8604fa6ddd1604ad6cbeee0d8babcfe2
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-05-15 23:53:38 +0800

    added conditional loading for comments too, also made a basic layout for the posts, could add a nav bar and side bar, make the like feature interactive

--- (days)/src/routes/(days)/day23/+page.svelte
+++ (days)/src/routes/(days)/day23/+page.svelte
@@ -5,6 +5,7 @@
 
     let posts = data.elfPosts
     let postedPosts: ElfPost[] = []
+    $: displayedPosts = postedPosts.reverse()
 
     // filter by time
     let time = new Date().toLocaleTimeString()
@@ -30,5 +31,37 @@
 
 
 </script>
-
-<p>{JSON.stringify(postedPosts)}</p>
+<div class="flex flex-col w-full items-center">
+    {#each displayedPosts as post}
+    <div class="flex flex-col w-1/2 mb-9">
+        <div class="border-2 border-dashed rounded-xl p-3 mb-7">
+            <div class="text-2xl mb-4">
+                <p>{post.author}</p>
+            </div>
+            <div class="mb-3">
+                <p>{post.content}</p>
+            </div>
+            <div class="w-full flex justify-between">
+                <p>{post.likes} likes</p>
+                <p>{post.timestamp.substring(0, 10)} | {post.timestamp.substring(11)}</p>
+            </div>
+        </div>
+        <div>
+            {#each post.comments as comment}
+            {#if timeToSeconds(comment.timestamp.slice(11, 19)) < currSeconds}   
+            <div class="flex flex-col w-2/3 mb-9 pl-5 ml-5 border-l-2 border-dashed">
+                <div class="w-full flex justify-between text-lg mb-4">
+                    <p>{comment.author}</p>
+                    <p>{comment.timestamp.substring(0, 10)} | {comment.timestamp.substring(11)}</p>
+                </div>
+                <div>
+                    <p>{comment.content}</p>
+                </div>
+            </div>
+            {/if}
+            {/each}
+        </div>
+    </div>
+        
+    {/each}
+</div>
