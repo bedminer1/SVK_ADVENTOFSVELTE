8d7f35c5b56a881031fb6906687aaa19c8b2547e
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-05-18 22:21:09 +0800

    water challenge

---
+++ (days)/src/routes/(days)/(bonus)/water/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    let total = 0
+    let waterToAdd: string
+    const TARGET = 2000
+
+    function handleSubmit() {
+        total += Number(waterToAdd)
+        waterToAdd = ""
+    }
+</script>
+
+<div class="flex flex-col h-[90vh] justify-center items-center">
+    <form on:submit={handleSubmit}>
+        <span class="h3">Amount to be added (ml): </span>
+        <input type="text" class="input" bind:value={waterToAdd} placeholder="eg. 500"> 
+        <button></button>
+    </form>
+    <p class="h3 w-full mb-8">Total Amount Drank: {total} ml</p>
+    <button class="text-xl btn variant-ghost-primary mb-10" on:click={() => total = 0}>Reset</button>
+
+    {#if total >= TARGET}
+        <p class="h3 text-center">TARGET HIT, NICE ONE!</p>
+    {/if}
+</div>
+
+
+
