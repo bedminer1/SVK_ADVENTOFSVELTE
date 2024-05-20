a4a5456f787c4e1eba681ca11fd76ea8215da653
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-05-20 23:42:00 +0800

    added progress bar and table

--- (days)/src/routes/(days)/(bonus)/water/+page.svelte
+++ (days)/src/routes/(days)/(bonus)/water/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import type { Water } from "three/examples/jsm/Addons.js";
+    import { Paginator, ProgressBar } from '@skeletonlabs/skeleton'
+    import type { PaginationSettings } from '@skeletonlabs/skeleton'
 
     let total = 0
     let waterToAdd: string
@@ -9,6 +10,10 @@
 
     async function handleSubmit() {
         const waterAmount = Number(waterToAdd)
+        if (isNaN(waterAmount)) {
+            waterToAdd = ""
+            return
+        }
         const dateTime = new Date().toLocaleString()
         const newInput: WaterInput = {
             timestamp: dateTime,
@@ -19,6 +24,18 @@
         total += waterAmount
         waterToAdd = ""
     }
+
+    let paginationSettings = {
+        page: 0,
+        limit: 10,
+        size: inputs.length,
+        amounts: [3, 5, 10, 15]
+    } satisfies PaginationSettings
+
+    $: paginatedSource = inputs.slice(
+        paginationSettings.page * paginationSettings.limit,
+        paginationSettings.page * paginationSettings.limit + paginationSettings.limit
+    );
 
     // TODO
     /*
@@ -36,23 +53,44 @@
         <button></button>
     </form>
 
+    <ProgressBar 
+        value={total} 
+        max={2000} 
+        height={"h-5"}
+        meter={total > 200 ? 'bg-[#DCC7EA]' : 'bg-white'}
+        class="mb-5"
+    />
+
     <div class="w-full border-dashed border-2 rounded-lg flex flex-col p-3 mb-5">
         <p class="h3 mb-4">Total Amount Drank: {total} ml</p>
         <p class="h3 mb-2">Amount to Target: {TARGET - total > 0 ? TARGET - total : 0} ml</p>
     </div>
     <button class="text-xl btn variant-ghost-primary mb-10" on:click={() => total = 0}>Reset</button>
 
-
     {#if total >= TARGET}
         <p class="h3 text-center">TARGET HIT, NICE ONE!</p>
     {/if}
 
-    <div>
-        {#each inputs as input}
-            <p>{input.timestamp}</p>
-            <p>{input.amount}ml</p>
-        {/each}
-    </div>
+    {#if inputs.length} 
+    <table class="table mb-3">
+        <thead>
+            <tr>
+                <th>Time Stamp</th>
+                <th>Amount</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each paginatedSource as input}
+                <tr>
+                    <td>{input.timestamp}</td>
+                    <td>{input.amount}</td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+
+    <Paginator bind:settings={paginationSettings}/>
+    {/if}
 </div>
 
 
